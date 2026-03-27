58d9be2b7557a50b6100c1729a4824dabe70fec0
j-albento <joannamarie.albento@gmail.com>
2026-03-26 19:48:30 -0600

    Initial commit

---
+++ src/StackImplementation.java
@@ -0,0 +1,28 @@
+import java.util.EmptyStackException;
+
+public class StackImplementation<T> implements StackInterface<T>{
+    @Override
+    public void push(T newEntry) {
+
+    }
+
+    @Override
+    public T pop() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public T peek() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
